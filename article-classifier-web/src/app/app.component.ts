4ac8d260a7a92a080c7ecb46cb065e1766cd258e
Piotr Barabasz <barabasz.piotrek@gmail.com>
2024-01-21 11:00:33 +0100

    add ui

--- article-classifier-web/src/app/app.component.ts
+++ article-classifier-web/src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'article-classifier-web';
+  selectedInputType: string = 'text';
+  textInput: string = '';
+  selectedFile: File | null = null;
+  predictedCategory: string | null = null;
+
+
+  onFileSelected(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList) {
+      this.selectedFile = fileList[0];
+    }
+  }
+
+  predictCategory(): void {
+    if (this.selectedInputType === 'text') {
+      this.sendTextForPrediction(this.textInput);
+    } else if (this.selectedFile) {
+      this.sendFileForPrediction(this.selectedFile);
+    }
+  }
+
+  sendTextForPrediction(text: string): void {
+    // TODO
+  }
+
+  sendFileForPrediction(file: File): void {
+    // TODO
+  }
+
+  handlePredictionResult(category: string): void {
+    this.predictedCategory = category;
+  }
 }
